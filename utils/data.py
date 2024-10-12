52227891b6d0b09fead2fb55e3fcb70b83233bdf
DieVerwandlungTK <buri0901uni0701@icloud.com>
2024-10-12 23:08:46 +0900

    delete example

--- utils/data.py
+++ utils/data.py
@@ -70,9 +70,3 @@ class IrisDataLoader(DataLoader):
     
     def __len__(self):
         return len(self._features)
-
-if __name__ == "__main__":
-    loader = IrisDataLoader("data/iris/iris.data", 3, True)
-    
-    for features, targets in loader:
-        print(features, targets)
