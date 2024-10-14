f560a79a0e15133d0cf7511abe46fd0a795c49cd
DieVerwandlungTK <buri0901uni0701@icloud.com>
2024-10-14 22:39:37 +0900

    refactor

--- models/perceptron/perceptron.py
+++ models/perceptron/perceptron.py
@@ -1,23 +1,6 @@
 import numpy as np
 from models.model import Model
 
-class Perceptron(Model):
-    def __init__(self, n_inputs, learning_rate=0.01):
-        self.n_inputs = n_inputs
-        self.learning_rate = learning_rate
-        self.weights = np.random.rand(n_inputs)
-        self.bias = np.random.rand()
-        
-    def predict(self, x):
-        z = x@self.weights.T + self.bias
-        return 1.0 if z > 0 else 0.0
-    
-    def train(self, x, y):
-        y_hat = self.predict(x)
-        error = y - y_hat
-        self.weights += self.learning_rate * error * x
-        self.bias += self.learning_rate * error
-
 class MultiClassPerceptron(Model):
     def __init__(self, n_inputs, n_classes, learning_rate=0.01):
         self.n_inputs = n_inputs
@@ -35,5 +18,19 @@ class MultiClassPerceptron(Model):
     def train(self, x, y):
         y_hat = self.predict(x)
         error = y - y_hat
+        self.weights += self.learning_rate * error[:, None] * x
+        self.bias += self.learning_rate * error
+
+class Perceptron(MultiClassPerceptron):
+    def __init__(self, n_inputs, learning_rate=0.01):
+        super().__init__(n_inputs, 1, learning_rate)
+        
+    def predict(self, x):
+        z = x@self.weights.T + self.bias
+        return 1.0 if z[0] > 0 else 0.0
+    
+    def train(self, x, y):
+        y_hat = self.predict(x)
+        error = np.array([y - y_hat])
         self.weights += self.learning_rate * error[:, None] * x
         self.bias += self.learning_rate * error
