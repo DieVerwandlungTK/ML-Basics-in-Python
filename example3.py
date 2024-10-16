711fb261954dd9eb0ac4e2d914966f045784bc64
DieVerwandlungTK <buri0901uni0701@icloud.com>
2024-10-16 09:00:00 +0900

    add graph

--- example3.py
+++ example3.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from models.neural_network.adaline import MultiClassADALINE
 from utils.dataloaders import IrisDataLoader
@@ -12,7 +13,7 @@ train_set.shuffle()
 print(f'train set count: {len(train_set)}')
 print(f'test set count: {len(test_set)}')
 
-adaline = MultiClassADALINE(4, 3, 1e-4)
+adaline = MultiClassADALINE(4, 3, 5e-4)
 
 target_map = {
     'Iris-setosa': np.array([1, 0, 0], dtype=np.float64),
@@ -20,14 +21,34 @@ target_map = {
     'Iris-virginica': np.array([0, 0, 1], dtype=np.float64)
 }
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlabel('iters')
+ax.set_ylabel('loss')
+lines, = ax.plot([], [])
+
 NUM_EPOCHS = 500
+iters = 0
+losses = []
+min_loss, max_loss = 1e9, -1e9
 for epoch in range(NUM_EPOCHS):
     for x, y in train_set:
+        iters += 1
         target = []
         for i in range(len(y)):
             target.append(target_map[y[i]])
         target = np.array(target)
         adaline.train(x, target)
+
+        losses.append(adaline.loss(x, target))
+        min_loss = min(min_loss, losses[-1])
+        max_loss = max(max_loss, losses[-1])
+
+        lines.set_data(np.arange(1, iters+1), losses)
+        ax.set_xlim(0, iters+1)
+        ax.set_ylim(min_loss-0.5, max_loss+0.5)
+        plt.pause(1e-3)
+
     train_set.shuffle()
 
 correct = 0
